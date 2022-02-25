cb44979e4808f98c23187c62843390e033321d58
Jordy Kafwe <kfwjor001@myuct.ac.za>
2022-02-25 19:57:59 +0200

    add find method

--- src/VaccineArray.java
+++ src/VaccineArray.java
@@ -14,4 +14,21 @@ public class VaccineArray {
         records++;
     }
 
+    /**
+     * Searches the data (Vaccine array) for the given vaccine
+     * 
+     * @param vaccine the vaccine to look for in the array
+     * @return the vaccine object that has the vaccinations for the country 
+     * or null if it is not found
+     */
+    public Vaccine find(Vaccine vaccine) {
+        for (int i = 0; i < records; i++) {
+            boolean isEqual = vaccine.compareTo(data[i]) == 0;
+            if (isEqual) {
+                return data[i];
+            }   
+        }
+        return null;
+    }
+
 }
